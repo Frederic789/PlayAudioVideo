c2529b0098c8202394509744c368ac1ddd0869ed
Freddy <fredericdouamba2014@gmail.com>
2023-11-29 15:02:40 -0800

    Initial Commit

---
+++ AudioVideo/AudioVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioVideoApp.swift
+//  AudioVideo
+//
+//  Created by Student Account on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
